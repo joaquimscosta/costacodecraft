714d70ee352038dc7fa0ff3dc2125ada544ab249
Joaquim Costa <jscosta88@gmail.com>
2024-01-19 15:26:43 -0500

    Update styles and navigation in Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
+import { useNavigate } from "react-router-dom";
 function Home() {
+  const navigate = useNavigate();
   return (
     <div className="greeting content">
       <h2>Hi, I am Joaquim</h2>
       <h3>Software Engineer</h3>
       <p className="read-more">
-        Read more <button>about me</button> or <button>contact me</button>
+        Read more <button onClick={() => navigate("/about")}>about me</button>{" "}
+        or <button onClick={() => navigate("/contact")}>contact me</button>
       </p>
     </div>
   );
