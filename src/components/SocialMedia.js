9e57142dbe452d6905a1e114607cab07b82a07b9
Joaquim Costa <jscosta88@gmail.com>
2024-03-26 15:43:17 -0400

    Add Tailwind CSS configuration files and update SocialMedia component

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,18 +1,14 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 function SocialMedia() {
   return (
     <nav className="social-media">
       <a href="https://github.com/joaquimscosta">
-        <FontAwesomeIcon icon={faGithub} size="2x" style={{ color: "#333" }} />
+        <FaGithub size="" style={{ color: "#333" }} />
       </a>
       <a href="https://www.linkedin.com/in/joaquimscosta">
-        <FontAwesomeIcon
-          icon={faLinkedin}
-          size="2x"
-          style={{ color: "#0077B5" }}
-        />
+        <FaLinkedin size="" style={{ color: "#0077B5" }} />
       </a>
     </nav>
   );
