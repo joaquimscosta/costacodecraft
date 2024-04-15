442bfbb6a574a9233aa83b5f8cb920bddc62d5e6
Joaquim Costa <jscosta88@gmail.com>
2024-04-15 16:48:39 -0400

    implement darkmode and responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import ThemeMode from "./ThemeMode";
 
 function Navbar() {
   const links = [
@@ -21,7 +22,37 @@ function Navbar() {
   });
   return (
     <nav className="navbar bg-neutral text-neutral-content">
-      {renderedLinks}
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost text-xl lg:hidden"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <div className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-neutral bg-opacity-80 rounded-box w-52">
+            {renderedLinks}
+          </div>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">{renderedLinks}</div>
+      {/* <div className="navbar-end">
+        <ThemeMode className="btn btn-ghost text-xl" />
+      </div> */}
     </nav>
   );
 }
