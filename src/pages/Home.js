e064e24739d8e109957e9ea211e88201ca943780
Joaquim Costa <jscosta88@gmail.com>
2024-05-30 15:10:15 -0400

    update home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,11 @@ function Home() {
   return (
     <div className="flex-1 self-center p-4 overflow-y-auto greeting">
       <div className="hero">
-        <div className="hero-content flex-col lg:flex-row">
-          <div className="max-w-md">
-            <h2 className="text-6xl font-bold">Hi, I am Joaquim</h2>
+        <div className="hero-content text-center">
+          <div className="max-w-lg">
+            <h2 className="text-6xl font-bold">Hi, I'm Joaquim. 👋</h2>
             <h3 className="text-4xl font-thin">Software Engineer</h3>
-            <p className="py-6 text-2xl read-more">
+            <p className="py-6 text-2xl tracking-wide read-more">
               Always Meeting End-User Requirements With Innovation. Read more
               <Link className="link link-info m-1" to={"/about"}>
                 about me.
