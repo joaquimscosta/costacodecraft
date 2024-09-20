1d3763dcb576e1f50b877e29b5cff24d12be5505
Joaquim Costa <jscosta88@gmail.com>
2024-09-20 12:49:29 -0400

    fix lint issue and add image vulnerability scan

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,18 @@ import Navbar from './Navbar'
 import PageTitle from './PageTitle'
 
 function Header({ ...rest }) {
-    const classes = classNames(rest.className, 'header')
-    return (
-        <>
-            <header {...rest} className={classes}>
-                {/* <h1 className="text-5xl lg:text-7xl font-bold brand">
+  const classes = classNames(rest.className, 'header')
+  return (
+    <>
+      <header {...rest} className={classes}>
+        {/* <h1 className="text-5xl lg:text-7xl font-bold brand">
           Costa Code Craft
         </h1> */}
-                <Navbar />
-            </header>
-            <PageTitle />
-        </>
-    )
+        <Navbar />
+      </header>
+      <PageTitle />
+    </>
+  )
 }
 
 export default Header
--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -2,21 +2,21 @@ import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 
 export default function PageTitle() {
-    const location = useLocation()
-    let [title, setTitle] = useState('')
-    useEffect(() => {
-        const newTitle = location.pathname
-        if (newTitle.length > 0) {
-            setTitle(newTitle.slice(1))
-        }
-    }, [location])
-
-    if (title) {
-        return (
-            <h2 className="capitalize divider divider-start pb-2 pl-2 text-5xl page-title">
-                {title}
-            </h2>
-        )
+  const location = useLocation()
+  let [title, setTitle] = useState('')
+  useEffect(() => {
+    const newTitle = location.pathname
+    if (newTitle.length > 0) {
+      setTitle(newTitle.slice(1))
     }
-    return <></>
+  }, [location])
+
+  if (title) {
+    return (
+      <h2 className="capitalize divider divider-start pb-2 pl-2 text-5xl page-title">
+        {title}
+      </h2>
+    )
+  }
+  return <></>
 }
