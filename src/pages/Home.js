a5b1aeb98b387c8d55c7d6376d127df2bbfb014d
Joaquim Costa <jscosta88@gmail.com>
2024-09-20 10:08:54 -0400

    Refactor ignore files and add prettier configuration

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 function Home() {
-  return (
-    <div className="flex-1 self-center p-4 overflow-y-auto greeting">
-      <div className="hero">
-        <div className="hero-content text-center">
-          <div className="max-w-lg">
-            <h2 className="text-6xl font-bold">👋 Hi, I'm Joaquim.</h2>
-            <h3 className="text-4xl font-thin">Software Engineer</h3>
-            <p className="py-6 text-2xl tracking-wide read-more">
-              Always Meeting End-User Requirements With Innovation. Read more
-              <Link className="link link-info m-1" to={"/about"}>
-                about me
-              </Link>
-            </p>
-          </div>
+    return (
+        <div className="flex-1 self-center p-4 overflow-y-auto greeting">
+            <div className="hero">
+                <div className="hero-content text-center">
+                    <div className="max-w-lg">
+                        <h2 className="text-6xl font-bold">
+                            👋 Hi, I'm Joaquim.
+                        </h2>
+                        <h3 className="text-4xl font-thin">
+                            Software Engineer
+                        </h3>
+                        <p className="py-6 text-2xl tracking-wide read-more">
+                            Always Meeting End-User Requirements With
+                            Innovation. Read more
+                            <Link className="link link-info m-1" to={'/about'}>
+                                about me
+                            </Link>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
 
-export default Home;
+export default Home
